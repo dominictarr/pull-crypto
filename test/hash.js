44d65b171ce672c5b2c7372132982dc49feb68ab
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-05 15:54:46 +0800

    update to new style

--- test/hash.js
+++ test/hash.js
@@ -2,12 +2,14 @@
 var hash = require('../')
 var pull = require('pull-stream')
 require('tape')('abc', function (t) {
-pull.values(['a', 'b', 'c'])
-  .pipe(hash(function (err, sum) {
+pull(
+  pull.values(['a', 'b', 'c']),
+  hash(function (err, sum) {
     if(err) throw err
     console.log(sum)
     t.equals(sum, 'a9993e364706816aba3e25717850c26c9cd0d89d')
     t.end()
-  }))
+  })
+)
 
 })
